2255ef84e99d81deab8ecad93c714c6fa2e86908
Adel Urazov <39420242+AdelUrazov@users.noreply.github.com>
2020-03-26 19:32:27 +0300

    Added vga_buffer writer

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,15 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Welcome to S.O.F.I.A Project";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print("Welcome to S.O.F.I.A Project   ");
     loop {}
 }
